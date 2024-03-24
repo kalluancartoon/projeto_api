faeeec32ac8a1781c1eb3d3c74bed25ba048afaa
Saraiva16 <103701978+Saraiva16@users.noreply.github.com>
2024-03-24 19:16:45 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'root');
+    define('PASS', '');
+    define('BASE', 'cadastro');
+
+    $conn = new mysqli(HOST, USER, PASS, BASE);
+
+    if ($conn->connect_errno) {
+        die("Erro na conexão com o banco de dados: " . $conn->connect_error);
+    }
+?>
